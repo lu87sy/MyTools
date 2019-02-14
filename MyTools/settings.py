d270ffedcc8191b42eac3071254d23e722aeed19
lu87sy <lu87sy@gmail.com>
2019-02-14 18:04:01 +0800

    创建models

--- MyTools/settings.py
+++ MyTools/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'MyTools.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mytools',
+        'HOST': '192.168.100.203',
+        'PORT': '7086',
+        'USER': 'root',
+        'PASSWORD': 'root'
     }
 }
 
@@ -105,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
--- login/models.py
+++ login/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    user_id = models.CharField(max_length=8, primary_key=True, verbose_name='用户id')
+    user_name = models.CharField(max_length=11, verbose_name='登录名')
+    password = models.CharField(max_length=128, verbose_name='密码')
+    slat = models.CharField(max_length=20, verbose_name='盐')
+    create_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+
+    def __str__(self):
+        return self.user_name
+
+
+
